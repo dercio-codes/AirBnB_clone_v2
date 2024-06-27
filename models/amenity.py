4632772d8f6706226c34f402eda604f01246313c
Dercio <12derciomaduna@gmail.com>
2024-06-26 18:49:16 -0700

    pusing new fixed tasks with executable

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,8 @@
-#!/usr/bin/python3
-"""Amenity module for the HBNB project"""
-
-from models.base_model import BaseModel, Base
-from models import HBNB_TYPE_STORAGE
 from sqlalchemy import Column, String
-
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 class Amenity(BaseModel, Base):
-    """Amenity class to store amenity information"""
     __tablename__ = 'amenities'
-
-    if HBNB_TYPE_STORAGE == 'db':
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
